7ab5e146cf6ca0478e447237865b89ecc1055d3c
AnhTuyet1992003 <21130606@st.hcmuaf.edu.vn>
2025-06-14 17:36:17 +0700

    Xu ly cac trang thai don hang ben admin va user

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ import EditImportOrder from "./pages/admin/edit-import-order";
 import ListProductNeedImport from "./pages/admin/list-product-need-import";
 import Coupon from './pages/user/coupon'
 import ChooseCoupon from './pages/user/choose-coupon'
+import ListOrder from './pages/admin/list-order'
+import OrderDetailAdmin from './pages/admin/order-detail'
 function App() {
   return (
       <Router>
@@ -102,7 +104,8 @@ function App() {
             <Route path="/list-import-order" element={<LayoutAdmin><ListImportOrder /></LayoutAdmin>} />
             <Route path="/edit-import-order/:id" element={<LayoutAdmin><EditImportOrder /></LayoutAdmin>} />
             <Route path="/list-product-need-import" element={<LayoutAdmin><ListProductNeedImport /></LayoutAdmin>} />
-
+            <Route path="/admin-list-order" element={<LayoutAdmin><ListOrder /></LayoutAdmin>} />
+            <Route path="/admin-order-detail" element={<LayoutAdmin><OrderDetailAdmin /></LayoutAdmin>} />
             {/*<Route path="/admin-list-user" element={<AdminListUser/>} />*/}
           {/*/!* Admin Routes *!/*/}
           {/*<Route path="/admin" element={<AdminHome />} />*/}
